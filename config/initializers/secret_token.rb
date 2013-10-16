651934055126d20fb10a3b257c13c9ac3cc8605e
apple <apple@appletekiMacBook-Air.local>
2013-10-16 23:32:15 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '54ce342eb4de290f43e7886fc02dca1c89caf779b46400acb43c614b2f37ffa0341756a7273c085304bdabc5d1532a3e37feb8aec7ebbddfc787702e7ad804c4'
